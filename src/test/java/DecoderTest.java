ae9cf5f72eeab1ccc0cf877239c6ff688ae5c5e4
Reto Hablützel <trash@rethab.ch>
2012-10-17 07:55:10 +0200

    re-wrote test so that it works with toString instead of object

--- src/test/java/DecoderTest.java
+++ src/test/java/DecoderTest.java
@@ -2,30 +2,28 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import ch.zhaw.powerpc.model.Decoder;
-import ch.zhaw.powerpc.model.instructions.ADDD;
 
 public class DecoderTest {
 
 	@Test
 	public void decodeCLR() {
-		// Assert.assertEquals(new CLR(0), Decoder.instructionDecode(p("0000001010000000")));
-		// Assert.assertEquals(new CLR(1), Decoder.instructionDecode(p("0000011011010101")));
-		// Assert.assertEquals(new CLR(2), Decoder.instructionDecode(p("0000101011111111")));
-		// Assert.assertEquals(new CLR(3), Decoder.instructionDecode(p("0000111011110000")));
+		Assert.assertEquals("CLR 0", d("0000001010000000"));
+		Assert.assertEquals("CLR 1", d("0000011011010101"));
+		Assert.assertEquals("CLR 2", d("0000101011111111"));
+		Assert.assertEquals("CLR 3", d("0000111011110000"));
 	}
 
 	@Test
 	public void decodeADDD() {
-		Assert.assertEquals(new ADDD(0), Decoder.instructionDecode(p("1000000000000000")));
-		Assert.assertEquals(new ADDD(1), Decoder.instructionDecode(p("1000000000000001")));
-		Assert.assertEquals(new ADDD(999), Decoder.instructionDecode(p("1011111111111111")));
-		Assert.assertEquals(new ADDD(-999), Decoder.instructionDecode(p("1100000000000000")));
-		Assert.assertEquals(new ADDD(-1), Decoder.instructionDecode(p("1111111111111111")));
-
+		Assert.assertEquals("ADDD #0", d("1000000000000000"));
+		Assert.assertEquals("ADDD #1", d("1000000000000001"));
+		Assert.assertEquals("ADDD #999", d("1011111111111111"));
+		Assert.assertEquals("ADDD #-999", d("1100000000000000"));
+		Assert.assertEquals("ADDD #-1", d("1111111111111111"));
 	}
 
-	private static int p(String bin) {
-		return Integer.parseInt(bin, 2);
+	private static String d(String bin) {
+		return Decoder.instructionDecode(Integer.parseInt(bin, 2)).toString();
 	}
 
 }
