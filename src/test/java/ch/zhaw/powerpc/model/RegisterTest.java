3a3e04a5571a7f7eccfa523d656a23f0ae78b827
Reto Hablützel <trash@rethab.ch>
2012-10-18 07:44:47 +0200

    test to make sure what is masked

--- src/test/java/ch/zhaw/powerpc/model/RegisterTest.java
+++ src/test/java/ch/zhaw/powerpc/model/RegisterTest.java
@@ -7,9 +7,18 @@ public class RegisterTest {
 
 	@Test
 	public void testMasking() {
-		Register accu = new Register();
-		accu.write(65536);
-		Assert.assertEquals("Accu should be 0 after setting it to 65536 since that's a 1 followed by 16 * 0", 0,
-				accu.read());
+		Register reg = new Register();
+		reg.write(65536);
+		Assert.assertEquals("Register should be 0 after setting it to 65536 since that's a 1 followed by 16 * 0", 0,
+				reg.read());
+	}
+	
+	@Test
+	public void testWriteAndRead() {
+		Register reg = new Register();
+		for (int i = -32768; i < 32767; i++) {
+			reg.write(i);
+			Assert.assertEquals(i + " should not be masked.", i, reg.read());
+		}
 	}
 }
