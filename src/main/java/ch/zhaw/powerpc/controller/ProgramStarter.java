d6ba739f9befa55595f2ff26441ddca66b863c75
des <des@xtyra.(none)>
2012-10-21 23:41:19 +0200

    added exclution of comment lines

--- src/main/java/ch/zhaw/powerpc/controller/ProgramStarter.java
+++ src/main/java/ch/zhaw/powerpc/controller/ProgramStarter.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import ch.zhaw.powerpc.model.ControlUnit;
+import ch.zhaw.powerpc.model.MainMemory;
+
 /**
  * Einstiegspunkt für das komplette Programm.
  * 
@@ -18,16 +21,36 @@ public class ProgramStarter {
 	String memory[];
 	// Ask for file
 	String inputFile;
+	int numberOfLines; // Including comment lines
+	int numberOfCodeLines; // Excludes comment lines
+	static int initialData[];
+	static ProgramStarter programStarter;
+	
+	static MainMemory ppcMainMemory;
+	static ControlUnit ppcControlUnit;
 
 	public static void main(String[] args) {
 		// read initial memory
-		// intialize control unit
-		
 		String currentInput = "/home/des/input.txt";
+		programStarter = new ProgramStarter(currentInput);
+		programStarter.String2IntParser();
+		programStarter.outputMemory();
+		
+		// intialize control unit
+		ppcMainMemory = new MainMemory(initialData);
+		ppcControlUnit = new ControlUnit(ppcMainMemory);
+
 		
 	//	new ControlUnit();
 		
-		new ProgramStarter(currentInput);
+
+		
+	}
+	
+	public void outputMemory() {
+		for (int i = 0; i < numberOfCodeLines; i++) {
+			System.out.println(memory[i]);
+		}
 		
 	}
 		
@@ -53,11 +76,40 @@ public class ProgramStarter {
 		FileReader fr = new FileReader(inputFile);
 		BufferedReader txtReader = new BufferedReader(fr);
 		
-		int numberOfLines = readLines();
-		String[] txtData = new String[numberOfLines];
+		readLines();
+		String[] txtData = new String[numberOfCodeLines];
+		
 		int i;
+		String line;
+		int firstNonSpaceIndex;
+		boolean firstNonSpaceFound;
+		int currentCodeLine = 0;
+		
+		
 		for (i=0; i< numberOfLines; i++) {
-			txtData[i] = txtReader.readLine();
+			firstNonSpaceIndex = 0;
+			firstNonSpaceFound = false;
+			
+			line = txtReader.readLine();
+			for (int j = 0; j < line.length() && firstNonSpaceFound == false; j++) {
+				firstNonSpaceIndex = j;
+				if (line.charAt(j) == ' ') {
+					continue;
+				}
+				firstNonSpaceFound = true;
+			}
+			if (firstNonSpaceFound == false){
+				continue;
+			}
+			else {
+				if (line.charAt(firstNonSpaceIndex) == '\'') {
+					continue;
+				}
+				else {
+					txtData[currentCodeLine] = line;
+					currentCodeLine++;
+				}		
+			}	
 		}
 		
 		txtReader.close();
@@ -65,26 +117,52 @@ public class ProgramStarter {
 		
 	}
 	
-	
-	
-	int readLines() throws IOException {
+	void readLines() throws IOException {
 		
 		FileReader file2Input = new FileReader(inputFile);
 		BufferedReader bf = new BufferedReader(file2Input);
 		
-		@SuppressWarnings("unused")
+
 		String begLine;
-		int amountLines = 0;
+		int firstNonSpaceIndex;
+		boolean firstNonSpaceFound;
+		numberOfCodeLines = 0;
+		numberOfLines = 0;
 		
 		while ((begLine = bf.readLine()) != null) {
-			amountLines++;
+			
+			firstNonSpaceIndex = 0;
+			firstNonSpaceFound = false;
+			
+			for (int i = 0; i < begLine.length() && firstNonSpaceFound == false; i++) {
+				firstNonSpaceIndex = i;
+				if (begLine.charAt(i) == ' ') {
+					continue;
+				}
+				firstNonSpaceFound = true;
+			}
+			if (firstNonSpaceFound == false) {
+				numberOfLines++;
+				continue;
+			}
+			else {
+				if (begLine.charAt(firstNonSpaceIndex) == '\'') {
+					numberOfLines++;
+					continue;
+				}
+				else {
+					numberOfCodeLines++;
+					numberOfLines++;
+				}
+				
+			}
 		}
 		bf.close();
 		
-		return amountLines;
 	}
 	
-		
-		
+	public boolean String2IntParser() {
+		return true;
+	}
 	
 }
