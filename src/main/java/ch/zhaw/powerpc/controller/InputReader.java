c8936cf00e9d15a03ce2f47838b5b10caa4f3940
des <des@xtyra.(none)>
2012-10-23 11:50:11 +0200

    refactoring further

--- src/main/java/ch/zhaw/powerpc/controller/InputReader.java
+++ src/main/java/ch/zhaw/powerpc/controller/InputReader.java
@@ -1,181 +1,67 @@
 package ch.zhaw.powerpc.controller;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 
-import ch.zhaw.powerpc.model.ControlUnit;
-import ch.zhaw.powerpc.model.MainMemory;
-
-//import ch.zhaw.powerpc.controller.InputInterpreter;
-
+/**
+ * Lese
+ * 
+ * @author des
+ * 
+ *
+ */
 public class InputReader {
-	
-	int numberOfLines; // Including comment lines
-	int numberOfCodeLines; // Excludes comment lines
-	String inputFile;
-	String stringMemory[];
-	
-	int numberOfMainMemoryIntegers; 
-	
-	static int memoryPointer;
-	static int initialData[];
-	static InputReader programStarter;
-	
-	static MainMemory ppcMainMemory;
-	static ControlUnit ppcControlUnit;
-	static Clock ppcClock;
-	
-	public InputReader(String inputFile) {
-		
-		this.inputFile = inputFile;
-		try {
-			stringMemory = OpenFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		/*InputInterpreter(stringMemory);
-		starter.InputInterpreter().trimStringMemory();
-		
-		starter.InputInterpreter().initializeInitialData();
-		starter.InputInterpreter().stringMemory2MainMemory();
-		
-		*/
-		
-		
+
+	private final BufferedReader inputBuffer;
+
+	public InputReader(String inputFile) throws FileNotFoundException {
+
+		FileReader fr = new FileReader(inputFile);
+		inputBuffer = new BufferedReader(fr);
 	}
 	
+	public InputReader(BufferedReader inputBuffer) {
+		this.inputBuffer = inputBuffer;
+	}
 
 	/**
-	 * @return txtData 
-	 * 					(Text aus dem InputFile)
+	 * @return txtData (Text aus dem InputFile)
 	 * @throws IOException
 	 * 
-	 * Einlesen des Files
+	 *             Einlesen des Files
 	 */
-	public String[] OpenFile() throws IOException{
-		FileReader fr = new FileReader(inputFile);
-		BufferedReader txtReader = new BufferedReader(fr);
-		
-		readLines();
-		String[] txtData = new String[numberOfCodeLines];
-		
-		int i;
-		String line;
-		int firstNonSpaceIndex;
-		boolean firstNonSpaceFound;
-		int currentCodeLine = 0;
-		
-		
-		for (i=0; i< numberOfLines; i++) {
-			firstNonSpaceIndex = 0;
-			firstNonSpaceFound = false;
-			
-			line = txtReader.readLine();
-			for (int j = 0; j < line.length() && firstNonSpaceFound == false; j++) {
-				firstNonSpaceIndex = j;
-				if (line.charAt(j) == ' ') {
-					continue;
-				}
-				firstNonSpaceFound = true;
-			}
-			if (firstNonSpaceFound == false){
-				continue;
-			}
-			else {
-				if (line.charAt(firstNonSpaceIndex) == '\'') {
-					continue;
-				}
-				else {
-					txtData[currentCodeLine] = line;
-					currentCodeLine++;
-				}		
-			}	
-		}
-		
-		txtReader.close();
-		return txtData;
-		
-	}
-	
-	/*
-	 *  bestimmen der Grösse des Arrays
-	 */
-	
-	void readLines() throws IOException {
-		
-		FileReader file2Input = new FileReader(inputFile);
-		BufferedReader bf = new BufferedReader(file2Input);
-		
+	public String[] OpenFile() throws IOException {
 
-		String begLine;
-		int firstNonSpaceIndex;
-		boolean firstNonSpaceFound;
-		numberOfCodeLines = 0;
-		numberOfLines = 0;
-		
-		while ((begLine = bf.readLine()) != null) {
-			
-			firstNonSpaceIndex = 0;
-			firstNonSpaceFound = false;
-			
-			for (int i = 0; i < begLine.length() && firstNonSpaceFound == false; i++) {
-				firstNonSpaceIndex = i;
-				if (begLine.charAt(i) == ' ') {
-					continue;
-				}
-				firstNonSpaceFound = true;
-			}
-			if (firstNonSpaceFound == false) {
-				numberOfLines++;
-				continue;
-			}
-			else {
-				if (begLine.charAt(firstNonSpaceIndex) == '\'') {
-					numberOfLines++;
-					continue;
-				}
-				else {
-					numberOfCodeLines++;
-					numberOfLines++;
-				}
-				
-			}
-		}
-		bf.close();
-		
-	}
+		LinkedList<String> liste = new LinkedList<String>();
 
-	/*
-	 * Test Output des eingelesenen Files
-	 */
-	
-	public void outputStringMemory() {
-		System.out.println("\nEingelesenes File:");
-		for (int i = 0; i < numberOfCodeLines; i++) {
-			System.out.println(stringMemory[i]);
-		}
-		
-	}
-	
-	/*
-	 * Test Output der Ausgabe an ControlUnit in Integer und als Binary
-	 */
-	
-	public void outputMainMemory() {
-		System.out.println("\nMainMemory als Integers:");
-		for (int i = 0; i < numberOfMainMemoryIntegers; i++) {
-			System.out.println(initialData[i]);
-		}
-		System.out.println("\nMainMemory als binary Integers (2 Codes per Integer):");
-		for (int i = 0; i < numberOfMainMemoryIntegers; i++) {
-			System.out.println("#"+memoryPointer +": " +Integer.toBinaryString(initialData[i]));
-			memoryPointer = memoryPointer + 2;
-		}
-	}
+		String line = this.inputBuffer.readLine();
 
+		while (line != null) {
+
+			int apostrophindex = line.indexOf('\'');
+
+			// Kommentar entfernen
+			if (apostrophindex != -1) {
+				line = line.substring(0, apostrophindex - 1);
+			}
+
+			// Leerzeilen rauslöschen
+			line = line.trim();
+
+			if (!line.isEmpty()) {
+				liste.add(line);
+			}
+
+			// gehe zur nächsten Zeile
+			line = this.inputBuffer.readLine();
+		}
+
+		this.inputBuffer.close();
+		return liste.toArray(new String[0]);
+
+	}
 
 }
