a85851cb758a76bdc324734e66500db9d647dbfc
Reto Hablützel <trash@rethab.ch>
2012-10-24 21:34:36 +0200

    comments for Assembler

--- src/main/java/ch/zhaw/powerpc/controller/Assembler.java
+++ src/main/java/ch/zhaw/powerpc/controller/Assembler.java
@@ -12,6 +12,12 @@ public final class Assembler {
 	 */
 	private static final String INSTR_PACKAGE = Instruction.class.getPackage().getName() + ".";
 
+	/**
+	 * Assembliert eine Liste vom mnemonischen Instruktionen.
+	 * 
+	 * @throws InvalidInstructionException
+	 *             wenn eine mnemonische Instruktion ungueltig ist
+	 */
 	public Instruction[] assemble(String[] mnemonics) throws InvalidInstructionException {
 		Instruction[] instructions = new Instruction[mnemonics.length];
 		for (int i = 0; i < mnemonics.length; i++) {
@@ -20,12 +26,26 @@ public final class Assembler {
 		return instructions;
 	}
 
+	/**
+	 * Assembliert eine mnemonische Instruktion.
+	 * 
+	 * @throws InvalidInstructionException
+	 *             wenn eine mnemonische Instruktion ungueltig ist.
+	 */
 	public Instruction assemble(String mnemonic) throws InvalidInstructionException {
 		String[] parts = mnemonic.split(" ");
+		// Der erste Teil der Instruktion muss der Klasse entsprechen
 		Class<Instruction> klass = loadClass(parts[0]);
+
+		// Der Rest sind Operanden. Je nach dem wie viele Operanden eine Instruktion hat, ist dieses Array
+		// unterschiedlich lang. Wenn es zwei Operanden hat, ist das Register immer vor der Adresse.
 		int[] ops = extractOperands(parts);
+
+		// Es wird angenommen, dass eine Instruktion genau einen Konstruktor besitzt. Dieser wird hier ermittelt.
 		Constructor<Instruction> constructor = getConstructor(klass);
 		try {
+
+			// Der Konstruktor wird mit den Operanden (Parameter fuer Konstruktor) instanziert.
 			return instantiate(constructor, ops);
 		} catch (Exception e) {
 			throw new InvalidInstructionException("Instruktion [" + mnemonic + "] kann nicht zugewiesen werden, weil: "
@@ -56,9 +76,14 @@ public final class Assembler {
 	}
 
 	private int[] extractOperands(String[] parts) {
+		boolean binaryOp = parts.length == 3; // ob die instruktion zwei parameter hat
 		int[] res = new int[parts.length - 1]; // erstes element ist klassenname
 		for (int i = 1; i < parts.length; i++) {
 			if (parts[i].startsWith("#")) {
+				if (binaryOp && i == 2) {
+					throw new InvalidInstructionException(
+							"Bei einer binaeren Instruktion ist die Adresse der zweite Parameter");
+				}
 				res[i - 1] = Integer.parseInt(parts[i].substring(1));
 			} else {
 				res[i - 1] = Integer.parseInt(parts[i]);
