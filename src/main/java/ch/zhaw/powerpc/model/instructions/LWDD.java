1af02720a4f28548736d29af68d1196275845e26
Reto Hablützel <trash@rethab.ch>
2012-10-24 21:51:46 +0200

    added LWDD instruction

---
+++ src/main/java/ch/zhaw/powerpc/model/instructions/LWDD.java
@@ -0,0 +1,38 @@
+package ch.zhaw.powerpc.model.instructions;
+
+import ch.zhaw.powerpc.model.ControlUnit;
+
+/**
+ * In das Register mit der Nummer xx (00 bis 11 fuer Akku, R-1, R-2, R-3) wird der Inhalt der Speicherzellen Adr und Adr
+ * + 1 (1 Wort = 2 Byte) geladen. Mit 10 Bit koennen 1KiB Speicher adressiert werden.
+ * 
+ * @author Reto
+ * 
+ */
+public class LWDD implements Instruction {
+
+	private final int register;
+
+	private final int address;
+
+	public LWDD(int register, int address) {
+		InstructionUtil.checkRegisterBounds(register);
+		InstructionUtil.checkAddressBounds(address);
+		this.register = register;
+		this.address = address;
+	}
+
+	@Override
+	public int run(ControlUnit controlUnit) {
+		int upperPart = controlUnit.getMemory().read(this.address);
+		int lowerPart = controlUnit.getMemory().read(this.address + 1);
+		controlUnit.getRegisters()[this.register].write((upperPart << 8) | lowerPart);
+		return controlUnit.getProgramCounter() + 1;
+	}
+
+	@Override
+	public String toString() {
+		return "LWDD " + this.register + " #" + this.address;
+	}
+
+}
