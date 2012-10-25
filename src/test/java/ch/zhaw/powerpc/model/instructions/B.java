5873963a9142adad678f62421d2a2b5b7f080cd8
mo0rhuhn <max-schrimpf@hotmail.com>
2012-10-25 23:59:51 +0200

    test für B

---
+++ src/test/java/ch/zhaw/powerpc/model/instructions/B.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ch.zhaw.powerpc.model.instructions;
+
+import ch.zhaw.powerpc.model.ControlUnit;
+
+/**
+ * @author Max
+ *
+ */
+public class B implements Instruction {
+
+	public B(int register) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see ch.zhaw.powerpc.model.instructions.Instruction#run(ch.zhaw.powerpc.model.ControlUnit)
+	 */
+	@Override
+	public int run(ControlUnit controlUnit) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
