7c2ed76884440cd4308837be6f3a29258e34abd3
Reto Hablützel <trash@rethab.ch>
2012-10-26 03:55:23 +0200

    refactored tests for lwdd

--- src/test/java/ch/zhaw/powerpc/model/instructions/TestUtil.java
+++ src/test/java/ch/zhaw/powerpc/model/instructions/TestUtil.java
@@ -1,30 +1,8 @@
 package ch.zhaw.powerpc.model.instructions;
 
 import static org.junit.Assert.assertEquals;
-import ch.zhaw.powerpc.model.ControlUnit;
-import ch.zhaw.powerpc.model.MainMemory;
 
 public final class TestUtil {
-
-	/**
-	 * Erstellt eine ControlUnit aus dem spzifizierten Array. Das Array ist pseudo - assoziativ, d.h. jeder zweite Wert
-	 * ist ein Index. Bsp:
-	 * 
-	 * <pre>
-	 * {501, 2, 502, 3} --> memory[501] = 2, memory[502] = 3
-	 * </pre>
-	 * 
-	 * @param vals
-	 * @return
-	 */
-	public static ControlUnit initCU(int... vals) {
-		byte[] initialMemory = new byte[600];
-		for (int i = 0; i < vals.length; i += 2) {
-			initialMemory[vals[i]] = (byte) vals[i + 1];
-		}
-		MainMemory mainMemory = new MainMemory();
-		return new ControlUnit(new MainMemory());
-	}
 
 	/**
 	 * Helper-Funktion, die sicherstellt, dass ein String (also eine binaere char folge) dem Wert vom char entspricht.
