faa8fbbc338707b7d7545c277e431bd5d8c26cd8
des <des@foxy.(none)>
2012-10-29 21:47:22 +0100

    SLA just started

---
+++ src/test/java/ch/zhaw/powerpc/model/instructions/SLATest.java
@@ -0,0 +1,28 @@
+package ch.zhaw.powerpc.model.instructions;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ch.zhaw.powerpc.model.ControlUnit;
+import ch.zhaw.powerpc.model.MainMemory;
+
+public class SLATest {
+	
+	@Test
+	public void shouldSLAData() {
+		
+		ControlUnit cu = new ControlUnit(new MainMemory());
+		
+		short[] validData = new short[] { 42 };
+		cu.getMemory().writeData(500, validData[0]);
+		new LWDD(0, 500).run(cu);
+		new SLA().run(cu);
+		Integer.toBinaryString(cu.getRegisters()[0].read());
+		assertEquals("101010", Integer.toBinaryString(cu.getRegisters()[0].read()));
+
+		
+		
+	}
+
+}
