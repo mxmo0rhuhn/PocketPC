98806700b9f7c4c7e0eb0d4b6d5ce0a333c06cc6
Reto Hablützel <trash@rethab.ch>
2012-11-02 23:08:18 +0100

    missing jumps

--- asmfiles/mul_reto.asm
+++ asmfiles/mul_reto.asm
@@ -6,32 +6,39 @@
 '                                     '
 ' 500 = op 1                          '
 ' 502 = op 2                          '
-' 504 = vorzeichen ( 1 = negativ)     '
+' 504 = vorzeichen ( 1 = negativ )    '
 ' 506 = restsumme lower               '
 ' 508 = restsumme upper               '
-' 510 = op 2 - upper
-' 520..540 = addresses                '
+' 510 = op 2 upper                    '
+' 520..540 = Adressen                 '
+' 560..580 = Konstanten               '
 '                                     '
 '''''''''''''''''''''''''''''''''''''''
 
-' OP 2 VORZEICHEN MERKEN
+'# INITIALIZATION
+	560=-32768                           ' 10..0 (Vorzeichen-Check)
+	562=1                                ' LSB-Check
+	LWDD 2 #562                          ' 1 im Reg-2
+
+'# OP 2 VORZEICHEN MERKEN
 	LWDD 0 #502
-	BZ                           ' GOTO END
-	SLL                                   ' Wenn links eine 1 rausfaellt, ist die Zahl negativ
-	BNC                          ' GOTO OP 1 VORZEICHEN
-	LWDD 0 #502                           ' Frisch laden
-	NOT                                   ' Mach Positiv
+	BZD                          ' GOTO END
+	LWDD 1 #560                          ' Wenn links eine 1 rausfaellt, ist die Zahl negativ
+	AND 1                                ' Mit 10..0 maskieren
+	BZ                           ' GOTO OP 1 VORZEICHEN MERKEN
+	LWDD 0 #502                          ' Frisch laden
+	NOT                                  ' Mach Positiv
 	INC
 	SWDD 0 #502
-	CLR
+	CLR 0                                ' Register leeren 
 	ADDD #1
 	SWDD 0 #504                          ' Merke Vorzeichen
 
 '# OP 1 VORZEICHEN MERKEN
 	LWDD 0 #500
-	BZ                           ' GOTO END
-	SLL                                  ' Wenn links eine 1 rausfaellt, ist die Zahl negativ
-	BNC                          ' GOTO MULTIPLICATION
+	BZD                          ' GOTO END
+	AND 1                                ' Mit 10..0 maskieren
+	BZD                          ' GOTO MULTIPLICATION
 	LWDD 0 #500
 	NOT                                  ' Mach Positiv
 	INC
@@ -41,47 +48,58 @@
 	SWDD 0 #504
 
 
-
-'# MULTIPLICATION
-	LWDD 0 #500
-	SRL                                 ' Zahl kann nicht negativ sein
-	BNC                          ' GOTO NULL-RECHTS
-
-'# EINS-RECHTS
-	DEC
-	SWDD 0 #500
-	LWDD 0 #502
-	LWDD 1 #506                        ' Lade restsumme-lower
-	ADD 1                              ' Addiere op 1 zu restsumme
-	SWDD 0 #506                        ' Speichere restsumme-upper
-	BNC                          ' GOTO MULTIPLICATION
-	LWDD 0 #508                        ' Lade restsumme-upper wegen overflow
-	INC
-	SWDD 0 #508
- 	BD                           ' GOTO MULTIPLICATION
+'# MULTIPLIKATION
+	LWDD 0 #500                         ' op 1 laden
+	SRL                                 ' setze rechtes bit als carry
+	BCD                          ' GOTO EINS-RECHTS
 
 '# NULL-RECHTS
 	SWDD 0 #500                        ' Wurde schon geschoben
 	LWDD 0 #502
 	SLL                                ' Multipliziere mit 2
 	SWDD 0 #502
+	BCD                          ' GOTO ADD-TO-UPPER (SKIP LINES)
 	LWDD 0 #510
-	SLL                                ' Erhoehe in jedem Fall (bei 0 passiert nix)
-gibs netBNC                          ' GOTO STORE UPPER (skip line)
-	OR 1                               ' 1 ist lower rausgefallen, fuege diese upper ein
-	SWDD 0 #510
-	BD                           ' GOTO MULTIPLICATION
+	SLL                                ' schiebe upper (bei 0 passiert nix)
+	BD                           ' GOTO STORE-UPPER (SKIP LINES)
+	LWDD 0 #510		           ' IAM ADD-TO-UPPER
+	SLL                                ' upper wird durch schieben erhoeht
+	OR 2                               ' wegen overflow inkrementieren
+	SWDD 0 #510                        ' IAM STORE-UPPER
+	BD                           ' GOTO MULTIPLIKATION
 
+'# EINS-RECHTS
+	LWDD 0 #500                        ' op 1 neu laden, weil wir nich schieben wollen
+	DEC                                ' nur eins entfernen
+	BZD                          ' GOTO LOWER-RESTSUMME-ADD
+	SWDD 0 #500                        ' wieder speichern
+	LWDD 0 #502                        ' lade op 2 lower
+	LWDD 1 #506                        ' lade restsumme lower
+	ADD 1                              ' addiere restsumme lower zu op 2 lower
+	SWDD 0 #506                        ' Speichere restsumme-upper
+	BCD                          ' GOTO INC-RESTSUMME-UPPER (SKIP LINES)
+	BD                           ' GOTO MULTIPLIKATION
+	LWDD 0 #508                        ' IAM INC-RESTSUMME-UPPER
+	INC
+	SWDD 0 #508
+	BD                           ' GOTO MULTIPLIKATION
+	
 
 '# LOWER RESTSUMME ZU OP 2 ADDIEREN
 	LWDD 0 #502                        ' op 2 - lower laden
 	LWDD 1 #506                        ' restsumme - lower laden
 	ADD 1                              ' addieren
 	SWDD 0 #502                        ' ergebnis - lower speichern
-	BNC                          ' GOTO UPPER RESTSUMME ZU OP 2 ADDIEREN
-	LWDD 0 #510                        ' op 2 - upper laden
-	INC                                ' uebertrag
-	SWDD 0 #510                        ' op 2 - upper speichern
+
+	BCD                          ' GOTO INC-UPPER-RESTSUMME
+	BD                           ' GOTO UPPER RESTSUMME ZU OP 2 UPPER ADDIEREN
+	LWDD 0 #510                        ' IAM INC-UPPER-RESTSUMME
+	INC
+	LWDD 1 #508                        ' grad im gleichen noch op2 upper addieren
+	ADD 1
+	SWDD 0 #510                        ' restsumme upper speichern
+	BD                           ' GOTO SETZE VORZEICHEN
+
 
 '# UPPER RESTSUMME ZU OP 2 ADDIEREN
 	LWDD 0 #510                        ' op 2 - upper laden
@@ -93,27 +111,23 @@ gibs netBNC                          ' GOTO STORE UPPER (skip line)
 
 '# SETZE VORZEICHEN
 	LWDD 0 #504
-	AND 1                             ' wenn 1, dann negativ. 2 oder 0 --> 0
-	BNZ                          ' GOTO END
+	AND 2                             ' im register 2 ist eine 1. wenn (x & 1 == 1) -> negativ
+	BZ                           ' GOTO END
 	LWDD 0 #502                       ' op 2 laden
 	NOT                               ' basiert auf der annahme, dass (* -1) auf beide seiten funktionieret
 	INC
 	SWDD 0 #502                       ' op 2 speichern
 	LWDD 0 #510                       ' op 2 - upper laden
-	NOT
-	INC                               ' op 2 - upper (* -1)
-	BC                           ' GOTO AFTER END (lol)
-	SWDD 0 #510
 
-
-'# THE FREAKING END
-	END
-
-
-'# NICHT NUR DIE WURST HAT ZWEI
+	BCD                          ' GOTO LOWER-INV-OVERFLOW
+	NOT                               ' beim op 2 lower invertieren gabs kein overflow
 	INC
 	SWDD 0 #510
+	BD                           ' GOTO END
+	NOT
+	ADDD #2                           ' beim op 2 lower invertieren gabs overflow, plus upper-post-invert-inc
+	SWDD 0 #510
 
 
-'# THE VERY END
+'# DEAD END
 	END
