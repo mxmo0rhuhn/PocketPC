98570c6d199ecd9f3947213771ebda80c8b18315
Reto Hablützel <trash@rethab.ch>
2012-11-03 19:15:32 +0100

    memory locations according to stern

--- asmfiles/mul_reto.asm
+++ asmfiles/mul_reto.asm
@@ -6,26 +6,26 @@
 '                                     '
 ' 500 = op 1                          '
 ' 502 = op 2                          '
-' 504 = vorzeichen ( 1 = negativ )    '
-' 506 = restsumme lower               '
-' 508 = restsumme upper               '
-' 510 = op 2 upper                    '
-' 520..540 = Adressen                 '
-' 560..580 = Konstanten               '
+' 504 = res lower                     '
+' 506 = res upper                     '
+' 508 = restsumme lower               '
+' 510 = restsumme upper               '
+' 512 = vorzeichen                    '
+' 520..540 = Konstanten               '
 '                                     '
 '''''''''''''''''''''''''''''''''''''''
 
 ' 100:INITIALIZATION
 	500=-181
 	502=180
-	560=-32768                           ' 10..0 (Vorzeichen-Check)
-	562=1                                ' LSB-Check
-	LWDD 2 #562                          ' 1 im Reg-2
+	520=-32768                           ' 10..0 (Vorzeichen-Check)
+	522=1                                ' LSB-Check
+	LWDD 2 #522                          ' 1 im Reg-2
 
 ' 102:OP 2 VORZEICHEN MERKEN
 	LWDD 0 #502
 	BZD #272                     ' GOTO END
-	LWDD 1 #560                          ' Wenn links eine 1 rausfaellt, ist die Zahl negativ
+	LWDD 1 #520                          ' Wenn links eine 1 rausfaellt, ist die Zahl negativ
 	AND 1                                ' Mit 10..0 maskieren
 	BZD #126                     ' GOTO OP 1 VORZEICHEN MERKEN
 	LWDD 0 #502                          ' Frisch laden
@@ -34,7 +34,7 @@
 	SWDD 0 #502
 	CLR 0                                ' Register leeren 
 	ADDD #1
-	SWDD 0 #504                          ' Merke Vorzeichen
+	SWDD 0 #512                          ' Merke Vorzeichen
 
 ' 126:OP 1 VORZEICHEN MERKEN
 	LWDD 0 #500
@@ -45,9 +45,9 @@
 	NOT                                  ' Mach Positiv
 	INC
 	SWDD 0 #500
-	LWDD 0 #504
+	LWDD 0 #512
 	ADDD #1                              ' Addiere Vorzeichen
-	SWDD 0 #504
+	SWDD 0 #512
 
 
 ' 148:MULTIPLIKATION
@@ -61,13 +61,13 @@
 	SLL                                ' Multipliziere mit 2
 	SWDD 0 #502
 	BCD #170                     ' GOTO ADD-TO-UPPER (SKIP LINES)
-	LWDD 0 #510
+	LWDD 0 #506                        ' lade res upper
 	SLL                                ' schiebe upper (bei 0 passiert nix)
 	BD #176                      ' GOTO STORE-UPPER (SKIP LINES)
-	LWDD 0 #510		           ' IAM ADD-TO-UPPER
+	LWDD 0 #506		           ' IAM ADD-TO-UPPER
 	SLL                                ' upper wird durch schieben erhoeht
 	OR 2                               ' wegen overflow inkrementieren
-	SWDD 0 #510                        ' IAM STORE-UPPER
+	SWDD 0 #506                        ' IAM STORE-UPPER
 	BD #148                      ' GOTO MULTIPLIKATION
 
 ' 180: EINS-RECHTS
@@ -76,55 +76,55 @@
 	BZD #208                     ' GOTO LOWER-RESTSUMME-ADD
 	SWDD 0 #500                        ' wieder speichern
 	LWDD 0 #502                        ' lade op 2 lower
-	LWDD 1 #506                        ' lade restsumme lower
+	LWDD 1 #508                        ' lade restsumme lower
 	ADD 1                              ' addiere restsumme lower zu op 2 lower
-	SWDD 0 #506                        ' Speichere restsumme-upper
+	SWDD 0 #508                        ' Speichere restsumme-lower
 	BCD #200                     ' GOTO INC-RESTSUMME-UPPER (SKIP LINES)
 	BD #148                      ' GOTO MULTIPLIKATION
-	LWDD 0 #508                        ' IAM INC-RESTSUMME-UPPER
+	LWDD 0 #510                        ' IAM INC-RESTSUMME-UPPER
 	INC
-	SWDD 0 #508
+	SWDD 0 #510
 	BD #148                      ' GOTO MULTIPLIKATION
 	
 
 ' 208:LOWER RESTSUMME ZU OP 2 ADDIEREN
 	LWDD 0 #502                        ' op 2 - lower laden
-	LWDD 1 #506                        ' restsumme - lower laden
+	LWDD 1 #508                        ' restsumme - lower laden
 	ADD 1                              ' addieren
 	SWDD 0 #502                        ' ergebnis - lower speichern
 	BCD #220                     ' GOTO INC-UPPER-RESTSUMME
 	BD #232                      ' GOTO UPPER RESTSUMME ZU OP 2 UPPER ADDIEREN
 	LWDD 0 #510                        ' IAM INC-UPPER-RESTSUMME
 	INC
-	LWDD 1 #508                        ' grad im gleichen noch op2 upper addieren
+	LWDD 1 #510                        ' restsumme upper laden und grad addieren
 	ADD 1
-	SWDD 0 #510                        ' restsumme upper speichern
+	SWDD 0 #506                        ' in res upper speichern
 	BD #240                      ' GOTO SETZE VORZEICHEN
 
 
 ' 232:UPPER RESTSUMME ZU OP 2 UPPER ADDIEREN
-	LWDD 0 #510                        ' op 2 - upper laden
-	LWDD 1 #508                        ' restsumme - upper laden
+	LWDD 0 #506                        ' res upper laden
+	LWDD 1 #510                        ' restsumme - upper laden
 	ADD 1
-	SWDD 0 #510                        ' op 2 - upper speichern
+	SWDD 0 #506                        ' res upper speichern
 	
 
 ' 240:SETZE VORZEICHEN
-	LWDD 0 #504
+	LWDD 0 #512
 	AND 2                             ' im register 2 ist eine 1. wenn (x & 1 == 1) -> negativ
 	BZD #272                     ' GOTO END
-	LWDD 0 #502                       ' op 2 laden
+	LWDD 0 #502                       ' res laden
 	NOT                               ' basiert auf der annahme, dass (* -1) auf beide seiten funktionieret
 	INC
-	SWDD 0 #502                       ' op 2 speichern
-	LWDD 0 #510                       ' op 2 - upper laden
+	SWDD 0 #502                       ' res speichern
+	LWDD 0 #506                       ' res upper laden
 	BCD #266                     ' GOTO LOWER-INV-OVERFLOW
-	NOT                               ' beim op 2 lower invertieren gabs kein overflow
+	NOT                               ' beim res lower invertieren gabs kein overflow
 	INC
-	SWDD 0 #510
+	SWDD 0 #506
 	BD #272                      ' GOTO END
 	NOT                               ' IAM LOWER-INV-OVERFLOW
-	ADDD #2                           ' beim op 2 lower invertieren gabs overflow, plus upper-post-invert-inc
+	ADDD #2                           ' beim res lower invertieren gabs overflow, plus upper-post-invert-inc
 	SWDD 0 #510
 
 
--- src/test/java/ch/zhaw/powerpc/TestMulReto.java
+++ src/test/java/ch/zhaw/powerpc/TestMulReto.java
@@ -5,22 +5,20 @@ import static org.junit.Assert.assertEquals;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import ch.zhaw.powerpc.controller.Assembler;
 import ch.zhaw.powerpc.controller.InputReader;
 import ch.zhaw.powerpc.controller.ProgramStarter;
 import ch.zhaw.powerpc.model.ControlUnit;
-import ch.zhaw.powerpc.model.MainMemory;
-import ch.zhaw.powerpc.model.instructions.Instruction;
 
 public class TestMulReto {
+
+	private static long stepsCounter;
+
+	private static long testCounter;
 
 	private static String[] initialInstructions;
 
@@ -30,38 +28,36 @@ public class TestMulReto {
 		initialInstructions = ir.readContents();
 	}
 
-//	@Test
-//	public void testZero() {
-//		for (int i = Short.MIN_VALUE; i <= Short.MAX_VALUE; i++) {
-//			assertEquals(0, multiply((short) i, (short) 0));
-//			assertEquals(0, multiply((short) 0, (short) i));
-//		}
-//	}
-
 	@Test
 	public void testNonOverflow() {
 		for (int i = Short.MIN_VALUE, j = Short.MAX_VALUE; i < j; i++, j--) {
-			if (Math.abs(i * j) < 32678) {
+			if (i * j >= Short.MIN_VALUE && i * j <= Short.MAX_VALUE) {
 				int res = multiply((short) i, (short) j);
-				assertEquals(String.format("%d (%s) * %d (%s) ", i, Integer.toBinaryString(i), j, Integer.toBinaryString(j)), i * j, res);
+				assertEquals(
+						String.format("%d (%s) * %d (%s) ", i, Integer.toBinaryString(i), j, Integer.toBinaryString(j)),
+						i * j, res);
 			}
 		}
 	}
 
-//	@Test
-//	public void testEven() {
-//		for (int i = Short.MIN_VALUE, j = Short.MAX_VALUE; i < j; i++, j--) {
-//			assertEquals(i * j, multiply((short) i, (short) j));
-//		}
-//	}
+	@AfterClass
+	public static void stats() {
+		System.out.println("\n******\n");
+		System.out.println("Run " + testCounter + " Tests");
+		System.out.println("On Average " + (stepsCounter / testCounter) + " Steps per Run");
+		System.out.println("\n******\n");
+	}
 
 	private int multiply(short a, short b) {
 		ControlUnit cu = new ControlUnit(ProgramStarter.createMainMemory(initialInstructions));
 		cu.getMemory().writeData(500, a);
 		cu.getMemory().writeData(502, b);
-		while (cu.runCycle())
-			;
-		return cu.getMemory().readData(510) << 16 + cu.getMemory().readData(502);
+		while (cu.runCycle()) {
+			stepsCounter++;
+		}
+		testCounter++;
+
+		return (cu.getMemory().readData(510) << 16) + cu.getMemory().readData(502);
 	}
 
 }
