eb7a17d896251ee39400b2305dad2517a0cdb63b
Reto Hablützel <trash@rethab.ch>
2012-11-04 01:28:19 +0100

    BUGFIX: upper restsumme immer updaten

--- asmfiles/mul_reto.asm
+++ asmfiles/mul_reto.asm
@@ -24,7 +24,7 @@
 
 ' 102:OP 2 VORZEICHEN MERKEN
 	LWDD 0 #502
-	BZD #272                     ' GOTO END
+	BZD #280                     ' GOTO END
 	LWDD 1 #520                          ' Wenn links eine 1 rausfaellt, ist die Zahl negativ
 	AND 1                                ' Mit 10..0 maskieren
 	BZD #126                     ' GOTO OP 1 VORZEICHEN MERKEN
@@ -38,7 +38,7 @@
 
 ' 126:OP 1 VORZEICHEN MERKEN
 	LWDD 0 #500
-	BZD #272                     ' GOTO END
+	BZD #280                     ' GOTO END
 	AND 1                                ' Mit 10..0 maskieren
 	BZD #148                     ' GOTO MULTIPLICATION
 	LWDD 0 #500
@@ -73,60 +73,64 @@
 ' 180: EINS-RECHTS
 	LWDD 0 #500                        ' op 1 neu laden, weil wir nich schieben wollen
 	DEC                                ' nur eins entfernen
-	BZD #208                     ' GOTO LOWER-RESTSUMME-ADD
+	BZD #216                     ' GOTO LOWER-RESTSUMME-ADD
 	SWDD 0 #500                        ' wieder speichern
 	LWDD 0 #502                        ' lade op 2 lower
 	LWDD 1 #508                        ' lade restsumme lower
 	ADD 1                              ' addiere restsumme lower zu op 2 lower
 	SWDD 0 #508                        ' Speichere restsumme-lower
-	BCD #200                     ' GOTO INC-RESTSUMME-UPPER (SKIP LINES)
+	LWDD 0 #506                        ' lade res upper
+	LWDD 1 #510                        ' lade restsumme upper
+	BCD #208                     ' GOTO INC-RESTSUMME-UPPER (SKIP LINES)
+	ADD 1                              ' addiere res zu restsumme
+	SWDD 0 #510                        ' schreibe neue restsumme
 	BD #148                      ' GOTO MULTIPLIKATION
-	LWDD 0 #510                        ' IAM INC-RESTSUMME-UPPER
-	INC
-	SWDD 0 #510
+	INC                          ' IAM INC-RESTSUMME-UPPER
+	ADD 1                              ' addiere res zu restsumme
+	SWDD 0 #510                        ' schreibe restsumme upper
 	BD #148                      ' GOTO MULTIPLIKATION
 	
 
-' 208:LOWER RESTSUMME ZU OP 2 ADDIEREN
+' 216:LOWER RESTSUMME ZU OP 2 ADDIEREN
 	LWDD 0 #502                        ' op 2 - lower laden
 	LWDD 1 #508                        ' restsumme - lower laden
 	ADD 1                              ' addieren
 	SWDD 0 #504                        ' res lower von op 2 uebernehmen. achtung grusig
-	BCD #220                     ' GOTO INC-UPPER-RESTSUMME
-	BD #232                      ' GOTO UPPER RESTSUMME ZU OP 2 UPPER ADDIEREN
+	BCD #228                     ' GOTO INC-UPPER-RESTSUMME
+	BD #240                      ' GOTO UPPER RESTSUMME ZU OP 2 UPPER ADDIEREN
 	LWDD 0 #510                        ' IAM INC-UPPER-RESTSUMME
 	INC
 	LWDD 1 #510                        ' restsumme upper laden und grad addieren
 	ADD 1
 	SWDD 0 #506                        ' in res upper speichern
-	BD #240                      ' GOTO SETZE VORZEICHEN
+	BD #248                      ' GOTO SETZE VORZEICHEN
 
 
-' 232:UPPER RESTSUMME ZU OP 2 UPPER ADDIEREN
+' 240:UPPER RESTSUMME ZU OP 2 UPPER ADDIEREN
 	LWDD 0 #506                        ' res upper laden
 	LWDD 1 #510                        ' restsumme - upper laden
 	ADD 1
 	SWDD 0 #506                        ' res upper speichern
 	
 
-' 240:SETZE VORZEICHEN
+' 248:SETZE VORZEICHEN
 	LWDD 0 #512
 	AND 2                             ' im register 2 ist eine 1. wenn (x & 1 == 1) -> negativ
-	BZD #272                     ' GOTO END
+	BZD #280                     ' GOTO END
 	LWDD 0 #504                       ' res laden
 	NOT                               ' basiert auf der annahme, dass (* -1) auf beide seiten funktionieret
 	INC
 	SWDD 0 #504                       ' res speichern
 	LWDD 0 #506                       ' res upper laden
-	BCD #266                     ' GOTO LOWER-INV-OVERFLOW
+	BCD #274                     ' GOTO LOWER-INV-OVERFLOW
 	NOT                               ' beim res lower invertieren gabs kein overflow
 	INC
 	SWDD 0 #506
-	BD #272                      ' GOTO END
+	BD #280                      ' GOTO END
 	NOT                               ' IAM LOWER-INV-OVERFLOW
 	ADDD #2                           ' beim res lower invertieren gabs overflow, plus upper-post-invert-inc
 	SWDD 0 #510
 
 
-' 272:DEAD END
+' 280:DEAD END
 	END
