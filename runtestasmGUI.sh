1015dbde29116bc287489ad071f971c931cf9f7c
mxmo0rhuhn <max-schrimpf@hotmail.com>
2014-12-30 23:44:12 +0100

    POM provided

--- runtestasmGUI.sh
+++ runtestasmGUI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mvn clean install
-java -jar target/PocketPC-0.1-SNAPSHOT-jar-with-dependencies.jar 
+java -Dasm=asmfiles/testasm -jar target/PocketPC-0.1-SNAPSHOT-jar-with-dependencies.jar 
